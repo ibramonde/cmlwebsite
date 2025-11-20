a598bc4e9068ed83dacf5f77ff6e3a7e122fe300
mohameb Ben Jaber <mohamed.bjaber@gmail.com>
2025-11-20 22:09:18 +0100

    cml godrink style

--- godrink/js/main.js
+++ godrink/js/main.js
@@ -1,10 +1,38 @@
+const panels = document.querySelectorAll('.panel');
+const panels_warrap = document.querySelectorAll('.panels')[0];
+
+panels.forEach(function(panel) {
+  panel.addEventListener('mouseenter', function() {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+ panels_warrap.addEventListener('mouseleave', function() {
+removeActiveClasses();
+ });
+
+function removeActiveClasses() {
+  panels.forEach(function(panel) {
+    panel.classList.remove('active');
+  });
+}
+
+
+
 const slides = document.querySelectorAll('.slide');
 const indicators = document.querySelectorAll('.indicator-index');
 
 function update(entries) {
+  const indicatorParent = document.querySelectorAll('.indicator')[0];
   entries.forEach((entry) => {
     const i = entry.target.dataset.index;
     indicators[i].classList.toggle('expand',entry.isIntersecting);
+    if( i== 0 && entry.isIntersecting ) {
+      indicatorParent.classList.add('first_slide');
+    }
+    else if( i==0 && !entry.isIntersecting ) {
+      indicatorParent.classList.remove('first_slide');
+    }
   });
 }
 
